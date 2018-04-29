d23320d3750b09220c7850ef72195caf53f8ceed
Lucas Cerqueira <lucas.cerqueira@poli.ufrj.br>
2018-04-29 19:52:08 -0300

    Created new activities and the user profile activity is based on its type (patient, caregiver).

--- app/src/main/java/com/lf/appcare/MainActivityCaregiver.java
+++ app/src/main/java/com/lf/appcare/MainActivityCaregiver.java
@@ -17,6 +17,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivityCaregiver extends AppCompatActivity {
 
@@ -41,6 +42,9 @@ public class MainActivityCaregiver extends AppCompatActivity {
         //get firebase auth instance
         auth = FirebaseAuth.getInstance();
         db = FirebaseDatabase.getInstance().getReference();
+
+        // Subscribe for notifications
+        FirebaseMessaging.getInstance().subscribeToTopic("android");
 
         //get current user
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
