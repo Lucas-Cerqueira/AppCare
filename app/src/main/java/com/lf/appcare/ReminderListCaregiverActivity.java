fc148fb22d21a3d9ce178f13485555a79031ee10
Lucas Cerqueira <lucas.cerqueira@poli.ufrj.br>
2018-05-22 20:48:52 -0300

    Fixed new user check, tweaked caregiver reminder list

--- app/src/main/java/com/lf/appcare/ReminderListCaregiverActivity.java
+++ app/src/main/java/com/lf/appcare/ReminderListCaregiverActivity.java
@@ -27,8 +27,13 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
@@ -42,9 +47,11 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
     private List<DataSnapshot> remindersSnapshot = new ArrayList<>();
     private List<Reminder> reminderList = new ArrayList<>();
     private List<String> reminderKeys = new ArrayList<>();
+    private Map<String,String> patientNames = new HashMap<>();
     private FirebaseUser user;
     private AutoCompleteTextView textView;
     private ArrayAdapter<Reminder> arrayAdapterReminder;
+    String caregiverUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -104,8 +111,8 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
         }
 
         user = auth.getCurrentUser();
-        String userUid = user.getUid();
-        DatabaseReference ref = db.child("remoteReminders").child(userUid);
+        caregiverUid = user.getUid();
+        DatabaseReference ref = db.child("remoteReminders").child(caregiverUid);
         ref.addValueEventListener(new ValueEventListener()
         {
             @Override
@@ -115,17 +122,30 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
                 reminderList = new ArrayList<>();
                 reminderKeys = new ArrayList<>();
                 remindersSnapshot = Lists.newArrayList (dataSnapshot.getChildren().iterator());
+
+                List<String> patientUids = new ArrayList<>();
                 for (DataSnapshot reminderSnapshot : remindersSnapshot)
                 {
                     Reminder reminder = reminderSnapshot.getValue(Reminder.class);
-                    reminderList.add (reminder);
-                    reminderKeys.add (reminderSnapshot.getKey());
+                    // Check if it is a "ONCE" alarm and it has ringed already
+                    String reminderDate = reminder.getDate();
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm", java.util.Locale.getDefault());
+                    String currentDate = dateFormat.format(Calendar.getInstance().getTime());
+                    System.out.println("Compare: " + reminderDate.compareTo(currentDate));
+                    if (reminder.getReminderType() == Reminder.ONCE && reminderDate.compareTo(currentDate) < 0)
+                    {
+                        System.out.println("Reminder passado");
+                        db.child("remoteReminders").child(caregiverUid).child(reminderSnapshot.getKey()).removeValue();
+                    }
+                    else
+                    {
+                        reminderList.add(reminder);
+                        reminderKeys.add(reminderSnapshot.getKey());
+                        patientUids.add(reminder.getUserUid());
+                    }
                 }
-                for (String key: reminderKeys)
-                {
-                    System.out.println("Key: " + key);
-                }
-                ListReminders();
+                GetPatientNames(patientUids);
+                //ListReminders();
             }
 
             @Override
@@ -159,9 +179,50 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void GetPatientNames(final List<String> patientUids)
+    {
+        if (!patientUids.isEmpty())
+        {
+            DatabaseReference ref = db.child("users");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot)
+                {
+                    List<DataSnapshot> usersSnapshot = Lists.newArrayList(dataSnapshot.getChildren().iterator());
+                    for (DataSnapshot userSnapshot : usersSnapshot)
+                    {
+                        AppCareUser user = userSnapshot.getValue(AppCareUser.class);
+                        String userUid = user.getUid();
+                        if (user.getUserType().equals(AppCareUser.PATIENT) && patientUids.contains(userUid))
+                        {
+                            patientNames.put(userUid, user.getFirstName());
+                            System.out.println("New patient found: " + user.getFirstName());
+                        }
+                    }
+
+                    ListReminders();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError)
+                {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+        }
+        // If there are no patient uids
+        // empty the hashmap
+        else
+        {
+            patientNames = new HashMap<>();
+            ListReminders();
+        }
+    }
+
     private void ListReminders ()
     {
-        if (reminderList == null)
+        if (reminderList == null || patientNames == null)
         {
             System.out.println("NOT REMINDERLIST REFERENCE");
             reminderListView.setVisibility(View.GONE);
@@ -169,6 +230,11 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
         else if (reminderList.isEmpty())
         {
             System.out.println("EMPTY REMINDERS LIST");
+            reminderListView.setVisibility(View.GONE);
+        }
+        else if (patientNames.isEmpty())
+        {
+            System.out.println("EMPTY PATIENT NAMES");
             reminderListView.setVisibility(View.GONE);
         }
         else
@@ -188,20 +254,24 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
                     TextView text3 = view.findViewById(R.id.reminderType);
                     TextView text4 = view.findViewById(R.id.patientName);
 
+                    Reminder reminder = reminderList.get(position);
+
                     //Getting date in a good format to read
                     String dateMatcher = "(\\d{4})(?:-)(\\d{2})(?:-)(\\d{2})(?:T)(.*)";
                     Pattern datePattern = Pattern.compile(dateMatcher);
                     Matcher date = datePattern.matcher(reminderList.get(position).getDate());
                     if(date.matches())
                     {
-                        String newDate = date.group(3) + "/" + date.group(2) + "/" + date.group(1) + " " + date.group(4);
-                        System.out.println(newDate);
+                        String newDate;
+                        if (reminder.getReminderType() == Reminder.DAILY)
+                            newDate = date.group(4);
+                        else
+                            newDate = date.group(3) + "/" + date.group(2) + "/" + date.group(1) + " " + date.group(4);
                         text2.setText(newDate);
                     }
 
                     text1.setText(reminderList.get(position).getName());
 
-                    System.out.println("tipo do reminder: " + reminderList.get(position).getReminderType());
                     switch (reminderList.get(position).getReminderType())
                     {
                         case Reminder.ONCE:
@@ -218,7 +288,7 @@ public class ReminderListCaregiverActivity extends AppCompatActivity {
                             break;
                     }
 
-                   text4.setText("Patient name");
+                   text4.setText(patientNames.get(reminder.getUserUid()));
 
                     return view;
                 }
